fa98359bb9bf919028bda0692e179d46df56ce4f
gemtugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2015-06-08 00:09:30 -0300

    Ejemplo de uso.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
+
 #include <string>
 #include <iostream>
+#include "Ascensor.h"
 
 using namespace std;
 
 int main() {
 
     cout << ".:: Ascensor ::." << endl;
+
+    Ascensor ascensorPrincipal(10);
+    Ascensor ascensorDeServicio(12);
+
+    cout << "El Ascensor principal se mueve entre PB y el piso " <<
+            ascensorPrincipal.obtenerUltimoPiso() << endl;
+
+    cout << "El Ascensor de servicio se mueve entre PB y el piso " <<
+            ascensorDeServicio.obtenerUltimoPiso() << endl;
+
 
     return 0;
 }
